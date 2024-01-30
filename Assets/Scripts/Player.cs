95962f624e3f0821f829f8a1b7fe3049d166649a
ianrr03 <julianramierezr2003@gmail.com>
2024-01-30 12:13:01 +0100

    30/01/2024 agregando movimiento, error animaciones y acabando el segundo player, pag 24 mas o menos

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    float speed = 3.0f;
+    [SerializeField]
+    Rigidbody2D rig;
+    [SerializeField]
+    int jumpForce = 20;
+    [SerializeField]
+    Animator anim;
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        //Le damos velocidad pero tambien en el eje y, porque si no se quedaria parado
+        rig.velocity = (transform.right * speed * Input.GetAxis("Horizontal")) + (transform.up * rig.velocity.y);
+
+        if (rig.velocity.x > 0.1f ) //Cambiamos la imagen de movimiento
+            GetComponent<SpriteRenderer>().flipX= false;
+        else if(rig.velocity.x < 0.1f)
+            GetComponent<SpriteRenderer>().flipX = true;
+
         
+        if (Input.GetButtonDown("Jump")) //Añadimos el salto
+        {
+            rig.AddForce(transform.up * jumpForce);
+        }
+        //Añadimos la animacion
+        anim.SetFloat("velocityX", Mathf.Abs(rig.velocity.x));
+        anim.SetFloat("velocityY", Mathf.Abs(rig.velocity.y));
+       
     }
 }
