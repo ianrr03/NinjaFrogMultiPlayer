a7dc6a6c4c1172fd917640e51dd2476874f05562
ianrr03 <julianramierezr2003@gmail.com>
2024-01-31 11:22:50 +0100

    Error animaciones agregando parametros de true o false al correr, loop infinito run

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,19 +24,25 @@ public class Player : MonoBehaviour
         //Le damos velocidad pero tambien en el eje y, porque si no se quedaria parado
         rig.velocity = (transform.right * speed * Input.GetAxis("Horizontal")) + (transform.up * rig.velocity.y);
 
-        if (rig.velocity.x > 0.1f ) //Cambiamos la imagen de movimiento
-            GetComponent<SpriteRenderer>().flipX= false;
-        else if(rig.velocity.x < 0.1f)
-            GetComponent<SpriteRenderer>().flipX = true;
+        if (rig.velocity.x > 0.1f)
+        {//Cambiamos la imagen de movimiento
 
-        
+            GetComponent<SpriteRenderer>().flipX = false;
+            anim.SetBool("isRunning", true); //mio
+        }
+
+        else if (rig.velocity.x < 0.1f)
+            GetComponent<SpriteRenderer>().flipX = true;
+            anim.SetBool("isRunning", true); //mio
+
         if (Input.GetButtonDown("Jump")) //Añadimos el salto
         {
             rig.AddForce(transform.up * jumpForce);
+
         }
         //Añadimos la animacion
-        anim.SetFloat("velocityX", Mathf.Abs(rig.velocity.x));
-        anim.SetFloat("velocityY", Mathf.Abs(rig.velocity.y));
-       
+         anim.SetFloat("velocityX", Mathf.Abs(rig.velocity.x));
+         anim.SetFloat("velocityY", Mathf.Abs(rig.velocity.y));
+
     }
 }
